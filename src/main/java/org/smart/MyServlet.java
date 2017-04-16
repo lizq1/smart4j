2fecf36c5093cc356a70ca4470565850c0ef84b7
li <524671754@qq.com>
2017-04-16 15:37:07 +0800

    smart4j

--- src/main/java/org/smart/MyServlet.java
+++ src/main/java/org/smart/MyServlet.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 public class MyServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("currentTime","2017415aaa");
+        req.setAttribute("currentTime","2017415aaacc");
         req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req,resp);
     }
 }
