3a86fe94ae0cc346806d5ae4cc8f6e7521f46b99
li <524671754@qq.com>
2017-04-16 15:11:15 +0800

    smart4j

---
+++ src/main/java/org/smart/MyServlet.java
@@ -0,0 +1,21 @@
+package org.smart;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by lenovo on 2017/4/15.
+ */
+@WebServlet("/hello")
+public class MyServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("currentTime","2017415");
+        System.out.println("aaaa");
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req,resp);
+    }
+}
